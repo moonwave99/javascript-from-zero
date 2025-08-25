a94db708ad115a70952203ae70da81954574d031
moonwave99 <diego.caponera@gmail.com>
2025-08-25 20:59:02 +0200

    Enforce dark theme for code blocks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,33 +1,33 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
-import { remarkSandpack } from 'remark-sandpack';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import { remarkSandpack } from "remark-sandpack";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 const config: Config = {
-  title: 'JavaScript From Zero',
-  tagline: 'Become an ace programmer!',
-  favicon: 'img/planet.svg',
-  url: 'https://moonwave99.github.com',
-  baseUrl: '/javascript-from-zero/',
-  organizationName: 'moonwave99',
-  projectName: 'javascript-from-zero',
+  title: "JavaScript From Zero",
+  tagline: "Become an ace programmer!",
+  favicon: "img/planet.svg",
+  url: "https://moonwave99.github.com",
+  baseUrl: "/javascript-from-zero/",
+  organizationName: "moonwave99",
+  projectName: "javascript-from-zero",
   trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          path: './lessons',
-          routeBasePath: 'lessons',
-          sidebarPath: './sidebars.ts',
+          path: "./lessons",
+          routeBasePath: "lessons",
+          sidebarPath: "./sidebars.ts",
           beforeDefaultRemarkPlugins: [remarkSandpack],
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
@@ -35,60 +35,60 @@ const config: Config = {
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'Home',
+      title: "Home",
 
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'lessonsSidebar',
-          position: 'left',
-          label: 'Lessons',
+          type: "docSidebar",
+          sidebarId: "lessonsSidebar",
+          position: "left",
+          label: "Lessons",
         },
-        { to: 'https://runjs.co', label: 'Code Playground', position: 'left' },
+        { to: "https://runjs.co", label: "Code Playground", position: "left" },
         {
-          to: '/about',
-          position: 'left',
-          label: 'About',
+          to: "/about",
+          position: "left",
+          label: "About",
         },
         {
-          href: 'https://github.com/moonwave99/javascript-from-zero',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/moonwave99/javascript-from-zero",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} <a href="https://www.diegocaponera.com">Diego Caponera</a>.`,
     },
     prism: {
-      theme: prismThemes.github,
+      theme: prismThemes.dracula,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
